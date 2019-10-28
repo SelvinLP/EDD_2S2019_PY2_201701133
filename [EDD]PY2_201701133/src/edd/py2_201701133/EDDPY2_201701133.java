8d92a3d0c8a0943747e786e1472c9e3bccae9a59
SelvinLP <36603066+SelvinLP@users.noreply.github.com>
2019-10-27 19:53:49 -0600

    Creacion del Proyecto

---
+++ [EDD]PY2_201701133/src/edd/py2_201701133/EDDPY2_201701133.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edd.py2_201701133;
+
+/**
+ *
+ * @author Aragon Perez
+ */
+public class EDDPY2_201701133 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login FMLogin=new Login();
+        FMLogin.show();
+    }
+    
+}
